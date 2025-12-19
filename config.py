e7e312e0ecbd593f8aadd1d03ad09069c44e7f1f
aelnagar1412-rgb <aelnagar1412@gmail.com>
2025-12-19 11:25:50 +0200

    Add configuration for Telegram bot and trading pairs

---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+TELEGRAM_TOKEN = "8570409684:AAEQBhKv0zMZaEXWcoCUGiJsKRspE5JuleM"
+TELEGRAM_CHAT_ID = "818760257"
+
+PAIRS = [
+    "EURUSD", "GBPUSD", "USDJPY",
+    "AUDUSD", "USDCHF", "USDCAD",
+]
+
+DEFAULT_TIMEFRAMES = ["1m", "5m", "15m"]
+
+PAIR_TIMEFRAMES = {
+    "XAUUSD": ["5m", "15m"],
+    "USDJPY": ["1m", "5m"]
+}
